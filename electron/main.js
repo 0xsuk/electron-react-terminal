4cf80d417cc6a58be397e5f9834dd9de9f151bff
0xsuk <ikeuu53@yahoo.co.jp>
2022-05-26 14:16:01 +0900

    Setup

---
+++ electron/main.js
@@ -0,0 +1,28 @@
+console.log("ELECTRON");
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+const { ipcMain } = require("electron/main");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadURL("http://localhost:3000");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
